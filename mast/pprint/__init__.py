5b6d5c6cb7300bc818df6be6baf458e18887df18
ilovetux <me@ilovetux.com>
2015-12-01 13:23:51 -0500

    laying out groundwork for outputters

--- mast/pprint/__init__.py
+++ mast/pprint/__init__.py
@@ -1,3 +1,7 @@
+import xml.etree.cElementTree as etree
+import json
+
+
 def pretty_print(elem, level=0):
     """
     pretty_print: I took this from several places on the internet
@@ -18,3 +22,49 @@ def pretty_print(elem, level=0):
     else:
         if level and (not elem.tail or not elem.tail.strip()):
             elem.tail = i
+
+
+class Outputer(object):
+    """
+    Outputer
+
+    An object which can take information and output it in various
+    formats.
+    """
+    def __init__(self, data):
+        self.data = data
+
+    def __json__(self):
+        if isinstance(self.data, dict) or isinstance(self.data, list):
+            return json.dumps(self.data)
+        elif isinstance(self.data, basestring):
+            return json.dumps(json.loads(self.data))
+        else:
+            raise NotImplementedError(
+                "Cannot convert {} to json".format(type(self.data)))
+
+    def __xml__(self):
+        if isinstance(self.data, basestring):
+            tree = etree.fromstring(self.data)
+            pretty_print(tree)
+            return etree.tostring(tree)
+        elif isinstance(self.data, list):
+            xml = "<root>{}</root>".format(
+                "".join(["<item>{}</item>".format(str(x)) for x in self.data]))
+            tree = etree.fromstring(xml)
+            pretty_print(tree)
+            return etree.tostring(tree)
+        elif isinstance(self.data, dict):
+            xml = "<root>{}</root>".format(
+                    "".join(["<{0}>{1}</{0}>".format(
+                        str(k), str(v)) for k, v in self.data.items()]))
+            tree = etree.fromstring(xml)
+            pretty_print(tree)
+            return etree.tostring(tree)
+        elif isinstance(self.data, etree.Element) \
+                or isinstance(self.data, etree.ElementTree):
+            pretty_print(self.data)
+            return self.data
+        else:
+            raise NotImplementedError(
+                "Cannot convert {} to xml".format(type(self.data)))
