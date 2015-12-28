3e0af338997ff398cf06593719199662ad7da3d1
ilovetux <me@ilovetux.com>
2015-12-28 15:13:16 -0500

    ENHANCEMENT: Fix docs

--- mast/pprint/__init__.py
+++ mast/pprint/__init__.py
@@ -1,13 +1,28 @@
+"""
+_module_: mast.pprint
+
+This module contains a single function which accepts an
+`xml.etree.ElementTree.Element` instance and inserts spaces
+to indent the xml.
+"""
 import xml.etree.cElementTree as etree
 import json
 
 
 def pretty_print(elem, level=0):
     """
-    pretty_print: I took this from several places on the internet
-        If you know where this originated, please email me at
-        ilovetux@ymail.com and I will provide proper credits
-        OPEN SOURCE RULES!!
+    _function_: `mast.pprint.pretty_print(elem, level=0)`
+
+    Insert spaces into an `xml.etree.ElementTree.Element` instance
+    in order to "pretty print" it.
+
+    Returns: None
+
+    Parameters:
+
+    * `elem`: The element to pretty-print
+    * `level`: FOR INTERNAL RECURSIVE USE. Do not set yourself. Controls
+    current indentation level.
     """
     i = "\n" + "  " * level
     if len(elem):
@@ -24,47 +39,49 @@ def pretty_print(elem, level=0):
             elem.tail = i
 
 
-class Outputer(object):
-    """
-    Outputer
-
-    An object which can take information and output it in various
-    formats.
-    """
-    def __init__(self, data):
-        self.data = data
-
-    def __json__(self):
-        if isinstance(self.data, dict) or isinstance(self.data, list):
-            return json.dumps(self.data)
-        elif isinstance(self.data, basestring):
-            return json.dumps(json.loads(self.data))
-        else:
-            raise NotImplementedError(
-                "Cannot convert {} to json".format(type(self.data)))
-
-    def __xml__(self):
-        if isinstance(self.data, basestring):
-            tree = etree.fromstring(self.data)
-            pretty_print(tree)
-            return etree.tostring(tree)
-        elif isinstance(self.data, list):
-            xml = "<root>{}</root>".format(
-                "".join(["<item>{}</item>".format(str(x)) for x in self.data]))
-            tree = etree.fromstring(xml)
-            pretty_print(tree)
-            return etree.tostring(tree)
-        elif isinstance(self.data, dict):
-            xml = "<root>{}</root>".format(
-                    "".join(["<{0}>{1}</{0}>".format(
-                        str(k), str(v)) for k, v in self.data.items()]))
-            tree = etree.fromstring(xml)
-            pretty_print(tree)
-            return etree.tostring(tree)
-        elif isinstance(self.data, etree.Element) \
-                or isinstance(self.data, etree.ElementTree):
-            pretty_print(self.data)
-            return self.data
-        else:
-            raise NotImplementedError(
-                "Cannot convert {} to xml".format(type(self.data)))
+# Work in progress (Leave commented out for production until ready)
+#
+#class Outputer(object):
+#    """
+#    Outputer
+#
+#    An object which can take information and output it in various
+#    formats.
+#    """
+#    def __init__(self, data):
+#        self.data = data
+#
+#    def __json__(self):
+#        if isinstance(self.data, dict) or isinstance(self.data, list):
+#            return json.dumps(self.data)
+#        elif isinstance(self.data, basestring):
+#            return json.dumps(json.loads(self.data))
+#        else:
+#            raise NotImplementedError(
+#                "Cannot convert {} to json".format(type(self.data)))
+#
+#    def __xml__(self):
+#        if isinstance(self.data, basestring):
+#            tree = etree.fromstring(self.data)
+#            pretty_print(tree)
+#            return etree.tostring(tree)
+#        elif isinstance(self.data, list):
+#            xml = "<root>{}</root>".format(
+#                "".join(["<item>{}</item>".format(str(x)) for x in self.data]))
+#            tree = etree.fromstring(xml)
+#            pretty_print(tree)
+#            return etree.tostring(tree)
+#        elif isinstance(self.data, dict):
+#            xml = "<root>{}</root>".format(
+#                    "".join(["<{0}>{1}</{0}>".format(
+#                        str(k), str(v)) for k, v in self.data.items()]))
+#            tree = etree.fromstring(xml)
+#            pretty_print(tree)
+#            return etree.tostring(tree)
+#        elif isinstance(self.data, etree.Element) \
+#                or isinstance(self.data, etree.ElementTree):
+#            pretty_print(self.data)
+#            return self.data
+#        else:
+#            raise NotImplementedError(
+#                "Cannot convert {} to xml".format(type(self.data)))
